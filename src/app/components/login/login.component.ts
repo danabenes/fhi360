4c629fdafb776e4186148c533c21e110c886cf2d
danabenes <danielle@lilypaddigital.co>
2022-08-19 17:57:14 +0800

    login validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Form, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { takeUntil} from 'rxjs/operators';
@@ -12,24 +12,33 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm = this.formBuilder.group({
-    email: '',
-    password: ''
-  });
+  loginForm: any;
+  error: boolean = false;
+  errorMessage: string = "";
 
   constructor(
     public router: Router,
     private apiService: ApiService,
     public formBuilder: FormBuilder
-  ) {}
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
   }
 
   login() {
-    this.apiService.postNoHeaders('auth', this.loginForm.value).subscribe((res) => {
+    this.apiService.postNoHeaders('auth', this.loginForm.value).subscribe(res => {
       localStorage.setItem('token', Object.values(res).toString());
       this.router.navigate(['home']);
+    },
+    err => {
+      console.log(err.error);
+      this.error = true;
+      this.errorMessage = err.error['password'];
     });
   }
 }
